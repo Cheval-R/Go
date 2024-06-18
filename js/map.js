46e6c9106dc944213967147e880187dafb8dd655
Cheval-R <rerisitdikov@gmail.com>
2024-06-18 19:00:30 +0300

    done

--- js/map.js
+++ js/map.js
@@ -4,9 +4,10 @@ async function initMap() {
 	// Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
 	await ymaps3.ready;
 
-	const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+	const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
 	// Import the package to add a default marker
 	const { YMapDefaultMarker } = await ymaps3.import('@yandex/ymaps3-markers@0.0.1');
+
 
 	// Иницилиазируем карту
 	const map = new YMap(
@@ -22,15 +23,19 @@ async function initMap() {
 				// Уровень масштабирования
 				zoom: 17
 			}
-		}
+		},
+		[
+			// Add a map scheme layer
+			new YMapDefaultSchemeLayer({}),
+			// Add a layer of geo objects to display the markers
+			new YMapDefaultFeaturesLayer({})
+		]
 	);
-	// const marker = new YMapDefaultMarker({
-	// 	coordinates: [37.565021, 55.723151],
-	// 	title: 'Game Over',
-	// 	color: '#008001'
-	// });
-	// map.addChild(marker)
 
-	// Добавляем слой для отображения схематической карты
-	map.addChild(new YMapDefaultSchemeLayer());
+	const marker = new YMapDefaultMarker({
+		coordinates: [37.565021, 55.723151],
+		title: 'Game Over',
+		color: '#6C0287'
+	});
+	map.addChild(marker);
 }
