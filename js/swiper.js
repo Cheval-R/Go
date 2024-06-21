8dab7e8f2de91bcc7a564e218778e1991d224d47
Cheval-R <rerisitdikov@gmail.com>
2024-06-21 10:24:56 +0300

    swiper done

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper', {
+	loop: true,
+
+	// autoplay: {
+	// 	delay: 4000,
+	// },
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.button-next',
+		prevEl: '.button-prev',
+	},
+});
